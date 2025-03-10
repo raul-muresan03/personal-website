8c4fac8597de6b49194b6527d33287cce66fac24
Raul <raulmuresancalin@gmail.com>
2025-03-10 18:30:18 +0200

    fix

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,21 +8,28 @@ export function AboutMe() {
                 <div className="flex justify-center items-center mt-20 text-lg">
                     <p>
                         <span>&gt; </span>
-                        3rd Year Computer Science student at Technical University of Cluj-Napoca.
-                        <br />
-                        <span>&gt; </span>
+                        I am a<span className="font-bold text-blue-500">&nbsp;programming instructor&nbsp;</span>at<span className="font-bold text-blue-500">&nbsp;IT Junior&nbsp;</span> (June 2024 – Present), where I create engaging presentations
+                        <div className="flex justify-center items-center">
+                            on <span className="font-bold text-blue-500">&nbsp;classical AI&nbsp;</span> and teach children programming languages such as <span className="font-bold text-blue-500">&nbsp;C, C++, and Python&nbsp;</span>.
+                        </div>
 
+                        <span>&gt; </span>
                         <span className="font-bold text-blue-500">Full Stack Developer</span> working on both <span className="font-bold text-blue-500">software (web apps and mobile apps)
                             <br />
                         </span>
                         <div className="flex justify-center items-center">
                             and
                             <span className="p-1 font-bold text-blue-500">hardware</span> (FPGA and circuits).
+                        </div>
 
+                        <span>&gt; </span>
+                        3rd Year <span className="font-bold text-blue-500">Computer Science</span> student at Technical University of Cluj-Napoca.
+                        <br />
+                        <div className="flex justify-center items-center">
                         </div>
                     </p>
                 </div>
-            </div>
-        </section>
+            </div >
+        </section >
     );
 }
